2235c68bb005573e54ce7ef2c6556e68d63d6d0a
marialarionova <33165612+marialarionova@users.noreply.github.com>
2018-05-09 22:01:06 +0300

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,12 @@
+﻿<?php
+$host = 'localhost';
+$user = 'root';
+$password = 'vertrigo';
+$database = 'k3143_lmv';
+$link = mysqli_connect($host, $user, $password, $database);
+if (!$link) {
+    die('Ошибка соединения: ' . mysql_error());
+}
+$link = mysqli_connect($host, $user, $password, $database) 
+    or die ("Ошибка подключения к базе данных" . mysqli_error());
+echo "Вы подключились!<br>";
